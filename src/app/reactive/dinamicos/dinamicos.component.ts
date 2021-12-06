7bd232df682b152a22890320dd629a810a9d1bb2
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-05 22:59:10 -0500

    Form Array

--- src/app/reactive/dinamicos/dinamicos.component.ts
+++ src/app/reactive/dinamicos/dinamicos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-dinamicos',
@@ -13,7 +13,15 @@ export class DinamicosComponent {
 
   miFormulario: FormGroup = this.fb.group({
     nombre: [ , [Validators.required , Validators.minLength(3)] ],
-  })
+    favoritos: this.fb.array( [
+      [ 'Metal', Validators.required],
+      [ ' Death Stranding ', Validators.required]
+    ], Validators.required)
+  });
+
+  get favoritosArr(){
+    return this.miFormulario.get('favoritos') as FormArray;
+  }
 
   campoEsValido(campo: string){
     return this.miFormulario.controls[campo].errors  && this.miFormulario.controls[campo].touched;
