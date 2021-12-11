f222972cb02de640bbb1fba39d565338b58475ed
Jorge Burgos <burgosjorge2907@gmail.com>
2021-12-10 22:08:42 -0500

    validacion nombre y email

--- src/app/auth/pages/registro/registro.component.ts
+++ src/app/auth/pages/registro/registro.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
@@ -8,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroComponent implements OnInit {
 
-  constructor() { }
+  nombreApellidoPattern: string = '([a-zA-Z]+) ([A-Za-z]+)';
+  emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['',[Validators.required, Validators.pattern(this.nombreApellidoPattern)]],
+    email: ['',[Validators.required, Validators.email, Validators.pattern(this.emailPattern) ]]
+  })
+
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
+
+    this.miFormulario.reset({
+      nombre: 'Jorge Burgos',
+      email: 'burgosjorge2907@gmail.com'
+    })
+  }
+
+  campoNoValido(campo: string){
+    return this.miFormulario.get(campo)?.invalid && this.miFormulario.get(campo)?.touched;
+  }
+
+  submitFormulario(){
+
+    console.log(this.miFormulario.value);
+
+    this.miFormulario.markAllAsTouched();
+
   }
 
 }
